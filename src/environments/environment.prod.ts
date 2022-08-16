5e311dfceae5dad7988e6ead7849f7e065e5ffbe
Maksym Ostapenko Kulieba <81162172+mikisimi25@users.noreply.github.com>
2022-08-16 11:28:20 +0200

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  tmdbApiURL: 'https://api.themoviedb.org',
-  tmdbApiToken: '2f37bc57c20e8ab508f6ce88a9ead705',
-  laravelApiURL: 'http://localhost:8000/api',
-};
