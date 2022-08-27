d93e93f5e7c942be0b404266f381c0ad673d1f33
most274 <mikisimi25.dev@gmail.com>
2022-08-27 12:37:54 +0200

    Env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  tmdbApiURL: 'https://api.themoviedb.org',
-  tmdbApiToken: '2f37bc57c20e8ab508f6ce88a9ead705',
-  laravelApiURL: 'http://localhost:8000/api',
+  tmdbApiURL: '',
+  tmdbApiToken: '',
+  laravelApiURL: '',
 };
 
 /*
